30de7a52be3a2deb1958176412ed665ac8d2a1c0
nataliesb17 <39168749+nataliesb17@users.noreply.github.com>
2019-09-25 19:55:30 -0400

    added sounds

---
+++ Assets/colliderNoise.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colliderNoise : MonoBehaviour
+{
+	
+	AudioSource audioSource;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+
+    void OnTriggerEnter(Collider other) 
+	{
+		audioSource.Play();
+	}
+	
+}
