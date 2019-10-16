0c801ca4d8504756463f560f9dc1a7ba2d4facd5
perfectharmonii <nsb29@njit.edu>
2019-10-16 13:48:21 -0400

    created a branch for the weapon

---
+++ Assets/Blaster Stuff/Scripts/Pool.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pool
+{
+   public static List<T> Create<T>(GameObject prefab, int count)
+        where T : MonoBehaviour
+    {
+        //New List
+
+        List<T> newPool = new List<T>();
+
+        //Create
+
+        return null;
+    }
+}
+
+public class ProjectilePool : Pool
+{
+   
+}
