392666158e5062febd1b4b8ca7febe28ba39f0fd
nataliesb17 <nsb29@njit.edu>
2019-11-11 14:50:33 -0500

    ghost despawns when contact with player

---
+++ Assets/destroyOnTouch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyOnTouch : MonoBehaviour
+{
+    public int playerDamage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+            Destroy(other.gameObject);
+            Debug.Log("Collision detected");
+            playerDamage += 1;
+        }
+    }
+}
