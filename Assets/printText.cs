7b97e73df2e9544c03ad6086205fe5722d3fa55f
Natalie <nataliesb17@gmail.com>
2019-11-12 19:40:27 -0500

    game over screen done yay

---
+++ Assets/printText.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class printText : MonoBehaviour
+{
+
+    private GameObject p;
+    private timerGame s;
+
+    public Text text;
+
+    int ghoulsHit;
+    int ghostsHit;
+    int playerHit;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        p = GameObject.Find("logging");
+        s = p.GetComponent<timerGame>();
+        ghostsHit = s.ghostsHit;
+        ghoulsHit = s.ghoulsHit;
+        playerHit = s.playerHit;
+
+        text.text = "Ghosts Killed: " + ghostsHit + ", Ghouls Hit: " + ghoulsHit + ", Player Damage: " + playerHit;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
